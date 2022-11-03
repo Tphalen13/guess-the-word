a051723ea4ad1b41fb178dd3dd0bb2f7d4dee765
Tiffany Phalen <tphalentech@gmail.com>
2022-11-03 14:34:31 -0600

    Created global variables, added function to add placeholders, and added event listener to guess button and prevented default refreshing.

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const placeholder = function(word){
+    const placeholderLetters = [];
+    for (const letter of word){
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+guessButton.addEventListener("click", function(e){
+    e.preventDefault();
+    const letterGuess = letterInput.ariaValueMax;
+    console.log(letterGuess);
+    letterInput.value = "";
+});
